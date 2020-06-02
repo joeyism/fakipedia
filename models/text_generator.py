2201d9db2029277460b832486dcf4be3e15ff9cd
joeyism <sham.joey@gmail.com>
2020-06-02 19:28:19 -0400

    Added parsing of new wiki

--- models/text_generator.py
+++ models/text_generator.py
@@ -14,21 +14,22 @@ tokenizer = GPT2Tokenizer.from_pretrained(GPT2_NAME)
 model = GPT2LMHeadModel.from_pretrained(GPT2_NAME)
 model = model.to(device)
 EOD_ID = tokenizer.encode("<|endoftext|>")
+NEW_LINE_ID = 198
+HEADER_ID = 796
 
 def generate_text(input_str, text_len=250, end_of_text_id=EOD_ID, top_random=10):
   cur_ids = torch.tensor(tokenizer.encode(input_str)).unsqueeze(0).long().to(device)
   model.eval()
   with torch.no_grad():
     for i in tqdm(range(text_len)):
-      try:
-        outputs = model(cur_ids[:, -1024:], labels=cur_ids[:, -1024:])
-      except:
-        import ipdb; ipdb.set_trace()
+      outputs = model(cur_ids[:, -1024:], labels=cur_ids[:, -1024:])
       loss, logits = outputs[:2]
       softmax_logits = torch.softmax(logits[0,-1], dim=0)
       next_token_id = choose_from_top(softmax_logits.to('cpu').numpy(), n=top_random)
       if next_token_id == end_of_text_id:
-          break
+        break
+      elif next_token_id == NEW_LINE_ID and cur_ids[-1] == HEADER_ID and cur_ids[-2] != HEADER_ID:
+        break
       cur_ids = torch.cat([cur_ids, torch.ones((1,1)).long().to(device) * next_token_id], dim=1)
     output_list = list(cur_ids.squeeze().to('cpu').numpy())
     output_text = tokenizer.decode(output_list)
--- scripts/setup_training.sh
+++ scripts/setup_training.sh
@@ -3,8 +3,15 @@
 MODEL_DIR=fakipedia_model_med
 GPT2_TYPE=gpt2-medium
 
+
 # setup instance
 sudo apt -y install unzip
+git clone https://github.com/NVIDIA/apex /tmp
+cd $_
+pip3 install -v --no-cache-dir ./
+cd -
+pip3 install --user -r requirements.txt
+pip3 install --user -r requirements-dev.txt
 
 # get data
 wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zip
@@ -16,6 +23,10 @@ python run_language_modeling.py \
 --model_name_or_path=$GPT2_TYPE \
 --do_train \
 --train_data_file=wikitext-2-raw/wiki.train.raw \
+--do_eval \
 --eval_data_file=wikitext-2-raw/wiki.valid.raw \
---per_gpu_train_batch_size=2 \
+--save_steps=-1 \
+--per_gpu_train_batch_size=1 \
 --num_train_epochs=5
+
+tar -czf $MODEL_DIR.tar.gz $MODEL_DIR
