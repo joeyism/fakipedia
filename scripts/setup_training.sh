475a037921a9f08dc13444a61a331a31d8a99900
joeyism <sham.joey@gmail.com>
2020-06-11 00:09:06 -0400

    Updated training

--- scripts/setup_training.sh
+++ scripts/setup_training.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export MODEL_DIR=fakipedia_distil
-export GPT2_TYPE=distilgpt2
-
-
 # setup instance
 sudo apt -y install unzip
 git clone https://github.com/NVIDIA/apex apex
@@ -16,18 +12,5 @@ pip3 install --user -r requirements-dev.txt
 wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zip
 unzip wikitext-2-raw-v1.zip
 
-python run_language_modeling.py \
---output_dir=$MODEL_DIR \
---model_type=$GPT2_TYPE \
---model_name_or_path=$GPT2_TYPE \
---do_train \
---train_data_file=wikitext-2-raw/wiki.train.raw \
---do_eval \
---eval_data_file=wikitext-2-raw/wiki.valid.raw \
---fp16 \
---fp16_opt_level=O2 \
---save_steps=-1 \
---per_gpu_train_batch_size=4 \
---num_train_epochs=3
-
-tar -czf $MODEL_DIR.tar.gz $MODEL_DIR
+# train
+bash scripts/setup_training.sh
---
+++ scripts/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export MODEL_DIR=fakipedia_distil
+export GPT2_TYPE=distilgpt2
+
+python run_language_modeling.py \
+--output_dir=$MODEL_DIR \
+--model_type=$GPT2_TYPE \
+--model_name_or_path=$GPT2_TYPE \
+--do_train \
+--train_data_file=wikitext-2-raw/wiki.train.raw \
+--do_eval \
+--eval_data_file=wikitext-2-raw/wiki.valid.raw \
+--save_steps=-1 \
+--per_gpu_train_batch_size=2 \
+--num_train_epochs=3
+
+tar -czf $MODEL_DIR.tar.gz $MODEL_DIR
